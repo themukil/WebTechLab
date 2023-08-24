7229416604d7e2f930bfab9e53f94dd699c5ee44
Mukilarasan V <mukilarasan.v@gmail.com>
2023-08-25 02:52:32 +0530

    Update Q4.java

--- Aug18/Q4.java
+++ Aug18/Q4.java
@@ -3,17 +3,12 @@ import java.util.Scanner;
 public class Q4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        try {
-            System.out.print("Enter an integer: ");
-            int num = scanner.nextInt();
-            double result = Math.exp(num);
-            System.out.printf("e^%d = %.6f%n", num, result);
-        } catch (java.util.InputMismatchException e) {
-            System.out.println("Invalid input. Please enter an integer.");
-        } finally {
-            scanner.close();
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
+        double result = Math.exp(num);
+        System.out.printf("e^%d = %.6f%n", num, result);
+      
+        scanner.close();
         }
     }
-}
 
